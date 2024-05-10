273f6456add6331afb2830b544fa4c591de38226
Theophilus Micah Bawa <theophilusmicah43@gmail.com>
2024-05-10 22:02:14 +0100

    added more code

--- scripts/index.js
+++ scripts/index.js
@@ -1,15 +1,10 @@
 const selection = document.querySelector('.subscribed');
 
-const update = selection.value;
+let update = selection.value;
 
 const updateAll = document.querySelector('.change');
 
 updateAll.addEventListener('click', () => {
-    document.querySelector('.container')
+   console.log(document.querySelector('.container')
         .innerHTML = `${update}`;
 });
-
-// function confirmSub() {
-//     document.querySelector('.containers')
-//         .innerHTML = `update`
-// }
